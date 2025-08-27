9ff9699679e269286b258b92a81fd21560d752ef
James Lamb <jlamb@nvidia.com>
2025-08-27 11:05:08 -0500

    simplify build scripts

--- ci/test_java.sh
+++ ci/test_java.sh
@@ -3,10 +3,6 @@
 
 set -euo pipefail
 
-EXITCODE=0
-trap "EXITCODE=1" ERR
-set +e
-
 rapids-logger "Check GPU usage"
 nvidia-smi
 
@@ -15,6 +11,3 @@ rapids-logger "Run Java build and tests"
 # TODO: switch to installing pre-built artifacts instead of rebuilding in test jobs
 #       ref: https://github.com/rapidsai/cuvs/issues/868
 ci/build_java.sh --run-java-tests
-
-rapids-logger "Test script exiting with value: $EXITCODE"
-exit ${EXITCODE}
