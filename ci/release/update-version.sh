4772ee5df9ff4689786684a558066dbf0cd63c32
Vivek Narang <vivek@searchscale.com>
2025-12-11 21:50:37 -0500

    Setup demo

--- ci/release/update-version.sh
+++ ci/release/update-version.sh
@@ -105,7 +105,9 @@ echo "${RAPIDS_BRANCH_NAME}" > RAPIDS_BRANCH
 # Update Java version
 NEXT_FULL_JAVA_TAG="${NEXT_SHORT_TAG}.${PATCH_PEP440}"
 sed_runner "s/VERSION=\".*\"/VERSION=\"${NEXT_FULL_JAVA_TAG}\"/g" build.sh
-sed_runner "/<!--CUVS_LUCENE#VERSION_UPDATE_MARKER_START-->.*<!--CUVS_LUCENE#VERSION_UPDATE_MARKER_END-->/s//<!--CUVS_LUCENE#VERSION_UPDATE_MARKER_START--><version>${NEXT_FULL_JAVA_TAG}<\/version><!--CUVS_LUCENE#VERSION_UPDATE_MARKER_END-->/g" pom.xml
+for FILE in pom.xml demo/pom.xml; do
+    sed_runner "/<!--CUVS_LUCENE#VERSION_UPDATE_MARKER_START-->.*<!--CUVS_LUCENE#VERSION_UPDATE_MARKER_END-->/s//<!--CUVS_LUCENE#VERSION_UPDATE_MARKER_START--><version>${NEXT_FULL_JAVA_TAG}<\/version><!--CUVS_LUCENE#VERSION_UPDATE_MARKER_END-->/g" "${FILE}"
+done
 
 sed_runner "s| CuVS [[:digit:]]\{2\}\.[[:digit:]]\{2\} | CuVS ${NEXT_SHORT_TAG} |g" README.md
 
@@ -117,3 +119,7 @@ for FILE in .github/workflows/*.yaml; do
   sed_runner "/shared-workflows/ s|@.*|@${RAPIDS_BRANCH_NAME}|g" "${FILE}"
   sed_runner "s/:[0-9]*\\.[0-9]*-/:${NEXT_SHORT_TAG}-/g" "${FILE}"
 done
+
+# Update versions in the demo readme file
+sed_runner "s/rapidsai\/ci-conda:[\.0-9]*-cuda/rapidsai\/ci-conda:${NEXT_SHORT_TAG}-cuda/g" "demo/README.md"
+sed_runner "s/target\/demo-[\.0-9]*-jar/target\/demo-${NEXT_FULL_JAVA_TAG}-jar/g" "demo/README.md"
