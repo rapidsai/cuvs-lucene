e68f5f4e65ba7a005b3dc7935cce635ae85e5831
Vivek Narang <vivek@searchscale.com>
2026-01-28 08:30:01 -0500

    Add tests

---
+++ src/test/java/com/nvidia/cuvs/lucene/TestAcceleratedHNSWParams.java
@@ -0,0 +1,105 @@
+/*
+ * SPDX-FileCopyrightText: Copyright (c) 2026, NVIDIA CORPORATION. All rights reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.nvidia.cuvs.lucene;
+
+import jakarta.validation.ConstraintViolationException;
+import java.util.Random;
+import java.util.logging.Logger;
+import org.apache.lucene.tests.util.LuceneTestCase;
+import org.apache.lucene.tests.util.LuceneTestCase.SuppressSysoutChecks;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+@SuppressSysoutChecks(bugUrl = "")
+public class TestAcceleratedHNSWParams extends LuceneTestCase {
+
+  @SuppressWarnings("unused")
+  private static final Logger log = Logger.getLogger(TestAcceleratedHNSWParams.class.getName());
+
+  private static Random random;
+
+  @Test
+  public void testAcceleratedHNSWParamsDefaultValues() {
+
+    AcceleratedHNSWParams params = new AcceleratedHNSWParams.Builder().build();
+
+    assertEquals(16, params.getBeamWidth());
+    assertEquals(64, params.getGraphdegree());
+    assertEquals(2, params.getHnswLayers());
+    assertEquals(128, params.getIntermediateGraphDegree());
+    assertEquals(8, params.getMaxConn());
+    assertEquals(1, params.getWriterThreads());
+  }
+
+  @Test
+  public void testAcceleratedHNSWParamsInvalidBeamWidth() {
+    for (int v :
+        new int[] {random.nextInt(Integer.MIN_VALUE, 1), random.nextInt(17, Integer.MAX_VALUE)}) {
+      assertThrows(
+          ConstraintViolationException.class,
+          () -> new AcceleratedHNSWParams.Builder().withBeamWidth(v).build());
+    }
+  }
+
+  @Test
+  public void testAcceleratedHNSWParamsInvalidGraphDegree() {
+    for (int v :
+        new int[] {random.nextInt(Integer.MIN_VALUE, 1), random.nextInt(65, Integer.MAX_VALUE)}) {
+      assertThrows(
+          ConstraintViolationException.class,
+          () -> new AcceleratedHNSWParams.Builder().withGraphDegree(v).build());
+    }
+  }
+
+  @Test
+  public void testAcceleratedHNSWParamsInvalidHNSWLayers() {
+    for (int v :
+        new int[] {random.nextInt(Integer.MIN_VALUE, 1), random.nextInt(3, Integer.MAX_VALUE)}) {
+      assertThrows(
+          ConstraintViolationException.class,
+          () -> new AcceleratedHNSWParams.Builder().withHNSWLayer(v).build());
+    }
+  }
+
+  @Test
+  public void testAcceleratedHNSWParamsInvalidIntGraphDegree() {
+    for (int v :
+        new int[] {random.nextInt(Integer.MIN_VALUE, 1), random.nextInt(129, Integer.MAX_VALUE)}) {
+      assertThrows(
+          ConstraintViolationException.class,
+          () -> new AcceleratedHNSWParams.Builder().withIntermediateGraphDegree(v).build());
+    }
+  }
+
+  @Test
+  public void testAcceleratedHNSWParamsInvalidMaxConn() {
+    for (int v :
+        new int[] {random.nextInt(Integer.MIN_VALUE, 1), random.nextInt(9, Integer.MAX_VALUE)}) {
+      assertThrows(
+          ConstraintViolationException.class,
+          () -> new AcceleratedHNSWParams.Builder().withMaxConn(v).build());
+    }
+  }
+
+  @Test
+  public void testAcceleratedHNSWParamsInvalidWriterThreads() {
+    for (int v :
+        new int[] {random.nextInt(Integer.MIN_VALUE, 1), random.nextInt(2, Integer.MAX_VALUE)}) {
+      assertThrows(
+          ConstraintViolationException.class,
+          () -> new AcceleratedHNSWParams.Builder().withWriterThreads(v).build());
+    }
+  }
+
+  @BeforeClass
+  public static void beforeClass() {
+    random = random();
+  }
+
+  @AfterClass
+  public static void afterClass() {}
+}
---
+++ src/test/java/com/nvidia/cuvs/lucene/TestGPUSearchParams.java
@@ -0,0 +1,90 @@
+/*
+ * SPDX-FileCopyrightText: Copyright (c) 2026, NVIDIA CORPORATION. All rights reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.nvidia.cuvs.lucene;
+
+import com.nvidia.cuvs.CagraIndexParams.CagraGraphBuildAlgo;
+import com.nvidia.cuvs.lucene.CuVS2510GPUVectorsWriter.IndexType;
+import jakarta.validation.ConstraintViolationException;
+import java.util.Random;
+import java.util.logging.Logger;
+import org.apache.lucene.tests.util.LuceneTestCase;
+import org.apache.lucene.tests.util.LuceneTestCase.SuppressSysoutChecks;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+@SuppressSysoutChecks(bugUrl = "")
+public class TestGPUSearchParams extends LuceneTestCase {
+
+  @SuppressWarnings("unused")
+  private static final Logger log = Logger.getLogger(TestGPUSearchParams.class.getName());
+
+  private static Random random;
+
+  @Test
+  public void testAcceleratedHNSWParamsDefaultValues() {
+
+    GPUSearchParams params = new GPUSearchParams.Builder().build();
+
+    assertEquals(64, params.getGraphdegree());
+    assertEquals(128, params.getIntermediateGraphDegree());
+    assertEquals(1, params.getWriterThreads());
+    assertEquals(CagraGraphBuildAlgo.NN_DESCENT, params.getCagraGraphBuildAlgo());
+    assertEquals(IndexType.CAGRA, params.getIndexType());
+  }
+
+  @Test
+  public void testGPUSearchParamsInvalidGraphDegree() {
+    for (int v :
+        new int[] {random.nextInt(Integer.MIN_VALUE, 1), random.nextInt(65, Integer.MAX_VALUE)}) {
+      assertThrows(
+          ConstraintViolationException.class,
+          () -> new GPUSearchParams.Builder().withGraphDegree(v).build());
+    }
+  }
+
+  @Test
+  public void testGPUSearchParamsInvalidIntermediateGraphDegree() {
+    for (int v :
+        new int[] {random.nextInt(Integer.MIN_VALUE, 1), random.nextInt(129, Integer.MAX_VALUE)}) {
+      assertThrows(
+          ConstraintViolationException.class,
+          () -> new GPUSearchParams.Builder().withIntermediateGraphDegree(v).build());
+    }
+  }
+
+  @Test
+  public void testGPUSearchParamsInvalidWriterThreads() {
+    for (int v :
+        new int[] {random.nextInt(Integer.MIN_VALUE, 1), random.nextInt(2, Integer.MAX_VALUE)}) {
+      assertThrows(
+          ConstraintViolationException.class,
+          () -> new GPUSearchParams.Builder().withWriterThreads(v).build());
+    }
+  }
+
+  @Test
+  public void testGPUSearchParamsInvalidCagraGraphBuildAlgo() {
+    assertThrows(
+        ConstraintViolationException.class,
+        () -> new GPUSearchParams.Builder().withCagraGraphBuildAlgo(null).build());
+  }
+
+  @Test
+  public void testGPUSearchParamsInvalidIndexType() {
+    assertThrows(
+        ConstraintViolationException.class,
+        () -> new GPUSearchParams.Builder().withIndexType(null).build());
+  }
+
+  @BeforeClass
+  public static void beforeClass() {
+    random = random();
+  }
+
+  @AfterClass
+  public static void afterClass() {}
+}
