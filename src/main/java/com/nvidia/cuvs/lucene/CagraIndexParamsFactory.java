75e5492517e83e5cdb48edbc8bb6be36ec38790e
Vivek Narang <vivek@searchscale.com>
2026-04-29 13:33:12 -0400

    Add internal notes and javadocs

--- src/main/java/com/nvidia/cuvs/lucene/CagraIndexParamsFactory.java
+++ src/main/java/com/nvidia/cuvs/lucene/CagraIndexParamsFactory.java
@@ -14,10 +14,20 @@ import com.nvidia.cuvs.CuVSIvfPqIndexParams;
 import com.nvidia.cuvs.CuVSIvfPqParams;
 import com.nvidia.cuvs.CuVSIvfPqSearchParams;
 
+/**
+ * A centralized approach to producing instances of {@link CagraIndexParams} based on the chosen strategy
+ */
 public class CagraIndexParamsFactory {
 
   private static final int ALGO_SWITCH_THRESHOLD = 5_000_000;
 
+  /**
+   * Translation of the internal logic found here:
+   * https://github.com/rapidsai/cuvs/blob/main/cpp/include/cuvs/neighbors/ivf_pq.hpp#L3385-L3428
+   *
+   * Ideally we should hook into the internal API but this is currently replicated to avoid complications
+   * in other parts of code base.
+   */
   private static CuVSIvfPqParams getCuVSIvfPqParams(long rows, long dimension) {
 
     int pqDim;
@@ -82,6 +92,10 @@ public class CagraIndexParamsFactory {
     return cuVSIvfPqParams;
   }
 
+  /*
+   * Rough translation from raft's internal utility found here:
+   * https://github.com/rapidsai/raft/blob/main/cpp/include/raft/util/integer_utils.hpp#L47-L56
+   */
   private static long roundUpSafe(long numberToRound, long modulus) {
     long remainder = numberToRound % modulus;
     if (remainder == 0) {
@@ -118,6 +132,14 @@ public class CagraIndexParamsFactory {
         .build();
   }
 
+  /**
+   * Creates an instance of {@link CagraIndexParams} based on the chosen strategy in the {@link GPUSearchParams}.
+   *
+   * @param gPUSearchParams an instance of {@link GPUSearchParams} containing input params incoming via the build and search on the GPU API.
+   * @param rows number of vectors in the data set
+   * @param dimension the dimension of the vectors in the data set
+   * @return an instance of {@link CagraIndexParams}
+   */
   public static CagraIndexParams create(
       GPUSearchParams gPUSearchParams, long rows, long dimension) {
     if (gPUSearchParams.getStrategy().equals(GPUSearchParams.Strategy.HEURISTIC)) {
@@ -147,10 +169,25 @@ public class CagraIndexParamsFactory {
     }
   }
 
+  /*
+   * Ideally there should be just one create method instead of two.
+   * We should do that when both the input parameter classes can be unified in the future.
+   */
+
+  /**
+   * Creates an instance of {@link CagraIndexParams} based on the chosen strategy in the {@link AcceleratedHNSWParams}.
+   *
+   * @param acceleratedHNSWParams an instance of {@link AcceleratedHNSWParams} containing input params incoming via the build and search on the GPU API.
+   * @param rows number of vectors in the data set
+   * @param dimension the dimension of the vectors in the data set
+   * @return an instance of {@link CagraIndexParams}
+   */
   public static CagraIndexParams create(
       AcceleratedHNSWParams acceleratedHNSWParams, long rows, long dimension) {
     if (acceleratedHNSWParams.getStrategy().equals(AcceleratedHNSWParams.Strategy.HEURISTIC)) {
-      if (rows < ALGO_SWITCH_THRESHOLD) {
+      if (rows
+          < ALGO_SWITCH_THRESHOLD) { // TODO: maybe consider making this threshold configurable from
+        // outside later.
         return getNNDescentParams(
             acceleratedHNSWParams.getGraphdegree(),
             acceleratedHNSWParams.getIntermediateGraphDegree(),
